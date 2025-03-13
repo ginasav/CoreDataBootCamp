fa982456f34c3d8db168284806acc3260f9fa126
Gina Saviano <gina.saviano.97@gmail.com>
2025-03-13 12:16:20 +0100

    Initial Commit

---
+++ CoreDataBootCamp/CoreDataBootCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataBootCampApp.swift
+//  CoreDataBootCamp
+//
+//  Created by Gina Saviano on 13/03/25.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataBootCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
